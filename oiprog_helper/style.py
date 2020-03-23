df4586879cf9793152c1a0e17642575e14e21063
Touch Sungkawichai <mb99n@Touchs-MacBook-Pro.local>
2020-03-23 16:08:59 +0700

    Themes

--- oiprog_helper/style.py
+++ oiprog_helper/style.py
@@ -10,20 +10,45 @@ msg = {
     "codeforces" : "let it be~"
 }
 
+theme = 'my99n'
+
+themes = {
+    'codeforces' : {
+        'text' : ('gray', 'bold'),
+        'number' : ('yellow', 'underline'),
+        'error' : ('red', 'bold'),
+        'symbol' : ':',
+        'chart' : [Yel, Blu, Red]
+    }, 
+    'my99n' : {
+        'text' : ('gray', 'bold'),
+        'number' : ('pink', 'underline'),
+        'error' : ('red', 'bold'),
+        'symbol' : '|',
+        'chart' : [ICya, IGre, IYel, IRed, IBlu, IPur]
+    },
+    'autoratch' : {
+        'text' : ('gray', 'bold'),
+        'number' : ('cyan', 'underline'),
+        'error' : ('red', 'bold'),
+        'symbol' : ':',
+        'chart' : [Gre, Whi, Cya, Whi]
+    }
+}
+
 def txt(str) :
-    return colorize(str, 'gray', 'bold')
+    return colorize(str, themes[theme]['text'][0], themes[theme]['text'][1])
 
 def number (str) :
-    return colorize(str, 'yellow', 'underline')
+    return colorize(str, themes[theme]['number'][0], themes[theme]['number'][1])
 
 @dc.writing(delay=0.02)
 def error () :
-    print(colorize("There's an error in the process, please config and try again", "red", "bold"))
+    print(colorize("There's an error in the process, please config and try again", themes[theme]['error'][0], themes[theme]['error'][1]))
 
 def chart (tochart) :
-    chart = Pyasciigraph(graphsymbol=':')
-    pattern = [Yel, Blu, Red]
-    data = vcolor(tochart, pattern)
+    chart = Pyasciigraph(graphsymbol=themes[theme]['symbol'])
+    data = vcolor(tochart, themes[theme]['chart'])
     for line in chart.graph(label=None, data=data):
         print(line)
 
